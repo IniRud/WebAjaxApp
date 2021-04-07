107e9c13b07081372a265221784011c1f43a3b45
iniekan <iniakpan.e@gmail.com>
2021-04-06 21:55:24 -0600

    Add project files.

---
+++ WebAjaxApp/Controllers/CustomerController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAjaxApp.Models;
+
+namespace WebAjaxApp.Controllers
+{
+    public class CustomerController : Controller
+    {
+        List<CustomerModel> customers = new List<CustomerModel>();
+        public CustomerController()
+        {
+            customers.Add(new CustomerModel { Id = 0, Name = "Ini", Age = 35});
+            customers.Add(new CustomerModel { Id = 1, Name = "Itoro", Age = 31 });
+            customers.Add(new CustomerModel { Id = 2, Name = "Junior", Age = 28 });
+            customers.Add(new CustomerModel { Id = 3, Name = "Patric", Age = 25 });
+            customers.Add(new CustomerModel { Id = 4, Name = "Dominic", Age = 15 });
+            customers.Add(new CustomerModel { Id = 5, Name = "Rita", Age = 25 });
+        }
+        public IActionResult Index()
+        {
+
+            return View(customers);
+        }
+    }
+}
---
+++ WebAjaxApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAjaxApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
